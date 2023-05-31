dc96a84c5945293bd74841a30df6753850bfed00
Danielle <dezevitz@gmail.com>
2023-05-30 20:15:58 -0400

    clear off extra code

--- src/App.js
+++ src/App.js
@@ -9,18 +9,6 @@ function App() {
     <ThemeProvider theme={theme}>
   `    <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Danielle's Website
-          </a>
           <HomeScreen theme={theme}/>
         </header>
       </div>
