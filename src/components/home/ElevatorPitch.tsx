621d8dd955805c28805aaea08c22f1652972bff3
Danielle Zevitz <53155697+dezevitz@users.noreply.github.com>
2023-06-28 18:37:52 -0400

    feature/FDT-34-init links on home (#17)

--- src/components/home/ElevatorPitch.tsx
+++ src/components/home/ElevatorPitch.tsx
@@ -1,16 +1,25 @@
 import BoxStyle from '../reusable/BoxStyle';
-import { ABOUT_ME, ELEVATOR_PITCH } from '../../utils/strings';
+import { ABOUT_ME, ELEVATOR_PITCH_END, ELEVATOR_PITCH_START, GITHUB, LINKEDIN } from '../../utils/strings';
 import React from 'react';
 import { Box } from '@mui/system';
 import HeaderStyle from '../reusable/HeaderStyle';
 import theme from '../../utils/theme';
+import { Link, Typography } from '@mui/material';
 
 function ElevatorPitch() {    
     return (
         <Box padding='20px'>
             <HeaderStyle text={ABOUT_ME} color={theme.palette.primary.light}/>
             <div style={{marginBottom: "20px"}}></div>
-            <BoxStyle text={ELEVATOR_PITCH}/>
+            <BoxStyle>
+                <Typography variant='body1' padding='10px'>
+                    { ELEVATOR_PITCH_START }
+                    <Link href={LINKEDIN} target="_blank" rel="noopener noreferrer" color="#3366CC">LinkedIn</Link> 
+                    { " and " }
+                    <Link href={GITHUB} target="_blank" rel="noopener noreferrer" color="#3366CC">Github</Link>
+                    { ELEVATOR_PITCH_END }
+                </Typography>
+            </BoxStyle> 
         </Box>
     )
 }
